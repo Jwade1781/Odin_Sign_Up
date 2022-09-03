33c0b04ffc9af5cfcf82bcdc7f43c2d1de4b6ed0
Jwade1781 <jwade5219@gmail.com>
2022-09-03 18:52:41 -0400

    Implemented background shifter

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
-function events () {
-    
+function main(){
+    backgroundShifter();
 }
+
+function backgroundShifter() {
+    const pauseMS = 5000;
+    const filePath = "../data/img";
+    const shiftingElement = document.querySelector("#leftContainer");
+
+    setInterval(() => {
+        shiftingElement.style.backgroundImage = `url("${filePath}/mountain${getShiftID()}.jpg")`
+    }, pauseMS);
+};
+
+function getShiftID() {
+    if (getShiftID.backgroundID === undefined) {
+        getShiftID.maxImages = 3;
+        getShiftID.backgroundID = 1;
+    }
+
+    else
+        getShiftID.backgroundID = ((getShiftID.backgroundID + 1) % getShiftID.maxImages) + 1
+
+    return getShiftID.backgroundID;
+}
+
+
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") main();
+})
